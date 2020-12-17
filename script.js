d6d80a06388fd2cfc43388ee4f97c53523d22d1b
HeyCi <acchevp@gmail.com>
2020-12-17 13:09:47 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var pictures = document.getElementById("pictures");
+for(let i = 0; i < 5 ; i++){
+    var picture = document.createElement("img");
+    picture.setAttribute("src", "https://picsum.photos/200");
+    pictures.append(picture);
+}
+
+
+//TODO adapter ça à ce que j'ai fait au-dessus
+// var json = JSON.parse(pictures.json);
+// var images = '';
+// for( var i=0; i < json.length; i++ ) {
+//     images += '<img src="' + json[i]['image_path'] + '" />';
+// }
+// document.getElementById( 'pictures' ).innerHTML = images;
